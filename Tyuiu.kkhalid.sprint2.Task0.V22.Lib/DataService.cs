609aef20310637db37a665e7699f92b8e58ca208
k khalid <khedayatullah452-lgtm>
2025-10-30 19:04:22 +0500

    Add project files.

---
+++ Tyuiu.kkhalid.sprint2.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.kkhalid.sprint2.Task0.V22.Lib
+{
+    public class DataService : ISprint2Task0V22
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y; // false
+            res[1] = x != y; // True
+            res[2] = x > y * 40; // True
+            res[3] = y > 100; // True 
+            res[4] = x >= 3105; // True
+            res[5] = y <= 76; // False
+            
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.kkhalid.sprint2.Task0.V22/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.kkhalid.sprint2.Task0.V22.Lib;
+
+namespace Tyuiu.kkhalid.sprint2.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 3105;
+            int y = 77;
+            bool[]res = new bool[6];
+            res = ds.GetCompareOperations(x, y);    
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" X = " + x);
+            Console.WriteLine(" Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ : False , True ,True ,True ,True ,False                        *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
+
+
+        
