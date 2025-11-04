eae576f1056049475794807261e604e8a7860de5
k khalid <khedayatullah452-lgtm>
2025-11-04 16:39:58 +0500

    Task3

--- Tyuiu.kkhalid.sprint2.Task3.V28.Lib/DataService.cs
+++ Tyuiu.kkhalid.sprint2.Task3.V28.Lib/DataService.cs
@@ -12,23 +12,36 @@ namespace Tyuiu.kkhalid.sprint2.Task3.V28.Lib
     {
         public double Calculate(double x)
         {
-           double y = 0;
+            double y = 0;
 
-            if (x > 0)
+            if (x > 1)
             {
-                y = ((x * x) + Math.Cos(x * x) - 11) / ((x * x) - Math.Sin(x * x) + 2);
+                y = ((x * x) + (Math.Cos(x * x) / (x - 1))) * Math.Pow(x, 3);
             }
             else
             {
-            }
-      
-            
 
-            return Math.Round(y, 3);
+                if (x == 0)
+                {
+                    y =( ((x * x) + Math.Cos(x * x) - 11) /( (x * x) - Math.Sin(x * x) + 2));
+                }
+                else
+                {
+
+                    return Math.Round(y, 3);
+                }
+
+            }
 
         }
 
 
-        
     }
+
 }
+
+
+
+        
+   
+
--- Tyuiu.kkhalid.sprint2.Task3.V28.Test/DataServiceTest.cs
+++ Tyuiu.kkhalid.sprint2.Task3.V28.Test/DataServiceTest.cs
@@ -8,18 +8,35 @@ namespace Tyuiu.kkhalid.sprint2.Task3.V28.Test
     [TestClass]
     public sealed class DataServiceTest
     {
+
         [TestMethod]
         public void ValidCondition1()
         {
             DataService ds = new DataService();
-           double x = 2;
-           double res = ds. Calculate(x);
+            double x = 2;
+            double res = ds.Calculate(x);
             double wait = -1.133;
             Assert.AreEqual(res, wait);
+        }
 
+
+
+
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+
+
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.000;
+            Assert.AreEqual(res, wait);
+        }
 
            
-        }
+        
 
 
 
